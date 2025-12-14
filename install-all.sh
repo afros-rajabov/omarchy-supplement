cc9fc8fa9c8a1b36aed799dc17675e294cb2f7bc
Afros Rajabov <afrosrajabov1207@mail.ru>
2025-12-14 23:30:11 +0700

    Fixed

--- install-all.sh
+++ install-all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install all packages in order
+# ./install-alacritty.sh
 ./install-fish.sh
 ./install-asdf.sh
-# ./install-alacritty.sh
 ./install-ghostty.sh
-# ./install-kitty.sh
+./install-kitty.sh
 ./install-tmux.sh
 ./install-stow.sh
 ./install-dotfiles.sh
