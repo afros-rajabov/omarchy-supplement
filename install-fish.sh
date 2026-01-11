231608bbf3644643687cf62e02f833d918c165ee
Afros Rajabov <afrosrajabov1207@mail.ru>
2026-01-12 03:47:48 +0700

    Install scripts

--- install-fish.sh
+++ install-fish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Install Zsh
-if ! command -v zsh &>/dev/null; then
-    yay -S --noconfirm --needed fish
-    sudo pacman -S omarchy-fish
-    omarchy-setup-fish
+# Install Fish
+if ! command -v fish &>/dev/null; then
+	yay -S --noconfirm --needed fish
+	sudo pacman -S omarchy-fish
+	omarchy-setup-fish
 fi
